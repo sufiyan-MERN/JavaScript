5a1fedb46b6b2cde6793d9693fa63a62f3b2aa8f
syed sufiyan ali <sufiyantdc@gmail.com>
2026-06-20 00:26:42 +0530

    day-10 done

---
+++ day-10/assignment.js
@@ -0,0 +1,45 @@
+// let heading = document.querySelector("#mainheading1");
+// console.log(heading);
+
+// let skills = document.querySelector("#skillslist");
+// console.log(skills);
+
+// let mainHeadin = document.querySelector("#mainheading1");
+// console.log(mainHeadin);
+
+// let descp = document.querySelector(".description");
+// console.log(descp);
+
+// let paradec = document.querySelectorAll(".description");
+// console.log(paradec);
+// console.log(paradec.length);
+
+// let sklist = document.querySelectorAll("#skillslist li");
+// console.log(sklist);
+
+// let introPara = document.querySelector("#intro");
+// console.log(introPara.innerText);
+
+// let first = document.querySelector("#decription1");
+// first.innerText = "i love learing javascript";
+// console.log(first);
+
+// let para = document.querySelectorAll(".description");
+// for (let i = 0; i < para.length; i++) {
+//   para[i].innerText += "-Updated*";
+// }
+// console.log(para[0]);
+
+// let skills = document.querySelectorAll(".skill li");
+// let myskill = `I have ${skills[2].innerText} skill!`;
+// console.log(skills);
+// console.log(myskill);
+
+// let obvDiv = document.querySelector("div");
+// console.log(obvDiv.innerHTML);
+// console.log(obvDiv.innerText);
+// console.log(obvDiv.textContent);
+
+let second = document.querySelectorAll(".description");
+second[1].innerText = "DOM manipulation is fun!.";
+console.log(second[1]);
---
+++ day-10/script.js
@@ -0,0 +1,54 @@
+// console.log("hello");
+// console.log(document);
+// console.dir(document);
+// let decp = document.getElementById("description");
+// console.log(decp);
+
+// let heading1 = document.getElementById("heading1");
+// console.log(heading1);
+
+// let img = document.getElementById("mainImages");
+// console.log(img);
+
+// let oldimages = document.getElementsByClassName("oldImg");
+// // console.log(oldimages[2]);
+// // console.log(oldimages[1]);
+// for (let i = 0; i < oldimages.length; i++) {
+//   console.log(oldimages[i]);
+// }
+
+// let boxLinks = document.getElementsByClassName("box");
+// console.log(boxLinks);
+
+// setInterval(() => {
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "red";
+//   }, 2000);
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "blue";
+//   }, 4000);
+// });
+
+// let paragraph = document.getElementsByTagName("p");
+// console.log(paragraph);
+// console.log(paragraph.length);
+
+// let images = document.getElementsByTagName("img");
+// console.log(images);
+
+// let heading = document.querySelector("#heading1");
+// console.log(heading);
+
+// let imgs = document.querySelectorAll(".oldImg");
+// console.log(imgs);
+
+// let para = document.querySelectorAll("p");
+// console.log(para);
+
+// let para = document.querySelector("#description");
+// console.log(para.innerHTML);
+// console.log(para.innerText);
+// console.log(para.textContent);
+
+// let heading = document.querySelector("#heading1");
+// heading.innerText = "our amazing solar system";
